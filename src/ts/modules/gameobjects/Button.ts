73ababdb476a0093f76643d2c671f56c979f078e
Ahmad Arsyel <arsyel@agate.id>
2021-01-26 05:06:51 +0700

    create button graphics implementation

---
+++ src/ts/modules/gameobjects/Button.ts
@@ -0,0 +1,22 @@
+import { Transform } from "./components/Transform";
+import { GraphicsButton, TextureInfo } from "./components/GraphicsButton";
+
+export class Button {
+
+	private _gameObject: GraphicsButton;
+	private _transform: Transform;
+
+	constructor (private _scene: Phaser.Scene, x: number, y: number, label: string, style?: Phaser.Types.GameObjects.Text.TextStyle, textureInfo?: TextureInfo) {
+		this._gameObject = new GraphicsButton(_scene, x, y, label, style, textureInfo);
+		this._transform = new Transform(this._gameObject.sprite);
+	}
+
+	get gameObject (): GraphicsButton {
+		return this._gameObject;
+	}
+
+	get transform (): Transform {
+		return this._transform;
+	}
+
+}
