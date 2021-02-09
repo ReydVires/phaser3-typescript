e5c8acf9cae95f61aff2b7473da2668144c3dfe3
Ahmad Arsyel <arsyel@agate.id>
2021-02-09 10:47:32 +0700

    update typedef pugin test

---
+++ src/types/toast-plugin.d.ts
@@ -0,0 +1,43 @@
+import { ToastPlugin } from "../ts/modules/test/ToastPlugin";
+
+export {};
+
+declare global {
+
+	namespace Phaser {
+
+		interface Scene {
+			toast: ToastPlugin
+		}
+
+		namespace Toast {
+
+			enum Position {
+				TOP_LEFT,
+				TOP_CENTER,
+				TOP_RIGHT,
+				MIDDLE_LEFT,
+				MIDDLE_CENTER,
+				MIDDLE_RIGHT,
+				BOTTOM_LEFT,
+				BOTTOM_CENTER,
+				BOTTOM_RIGHT,
+			}
+
+			type Config = {
+				showDuration: number,
+				manualClose: boolean
+			}
+
+			type TextureInfo = {
+				width: number,
+				height: number,
+				color: number,
+				radius: number,
+			}
+
+		}
+
+	}
+
+}
